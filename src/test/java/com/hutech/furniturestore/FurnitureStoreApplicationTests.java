9ac4792af708dcc8f428034ae8848d65e5b75b77
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2024-05-09 00:56:28 +0700

    feat: initial project

---
+++ src/test/java/com/hutech/furniturestore/FurnitureStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutech.furniturestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

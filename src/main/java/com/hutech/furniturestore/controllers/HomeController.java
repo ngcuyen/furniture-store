5ed7dedbf5168916b93122f3d84002026e233775
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2024-05-26 01:17:16 +0700

    feat: add structure frontend

--- src/main/java/com/hutech/furniturestore/controllers/HomeController.java
+++ src/main/java/com/hutech/furniturestore/controllers/HomeController.java
@@ -3,11 +3,14 @@ package com.hutech.furniturestore.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class HomeController {
     @GetMapping("/")
     public String hello() {
 //        model.addAttribute("message", "XIN CHÀO TRƯỜNG ĐẠI HỌC CÔNG NGHỆ THÀNH PHỐ HỒ CHÍ MINH!");
+
         return "Hello World";
     }
 }

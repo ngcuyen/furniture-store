1efc4d91bef13e05dabf0030389ba1c6f4df9b86
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2024-06-12 00:53:32 +0700

    feat: setup role swagger

---
+++ src/main/java/com/hutech/furniturestore/exceptions/NoSuchElementFoundException.java
@@ -0,0 +1,8 @@
+package com.hutech.furniturestore.exceptions;
+
+public class NoSuchElementFoundException extends RuntimeException {
+    public NoSuchElementFoundException(String message) {
+        super(message);
+    }
+}
+
